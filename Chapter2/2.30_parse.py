975a791e2ebcdd911560aa9e056db76d8a4bc7e2
Milan <milanmails@gmail.com>
2015-11-10 21:38:11 +0530

    Third commit

--- Chapter2/2.30_parse.py
+++ Chapter2/2.30_parse.py
@@ -1,8 +1,8 @@
-import sys
+# parse comma separated values
+# from a file.
 def parse_csv(f):
     fp = open(f, 'r')
-    a = fp.read()
-    d = a.split('\n')
-    d.pop()
-    return [x.split(',') for x in d]
-print(parse_csv('a.csv'))
+    a = fp.readlines()
+    print [x[:-1].split(',') for x in a]
+    return fp.close()
+parse_csv('a.csv')
--- Chapter3/prob3.2_extcount.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-def count(p):
-    freq  ={}
-    w = os.listdir(p).sort(key=lambda x: x.split('.')[1])
-    print(w)
-   # for item in os.listdir(p):
-   #     print       #  print item
-
-print (count(sys.argv[1]))
